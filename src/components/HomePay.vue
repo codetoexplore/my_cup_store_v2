7a7d2ca5ea47d6fe34f7e388a014e9fed4e45535
Giedrius Bernotavicius <xgiedrius@yahoo.com>
2019-06-03 21:48:56 +0100

    Pay function

--- src/components/HomePay.vue
+++ src/components/HomePay.vue
@@ -1,5 +1,7 @@
 <template>
-    <v-form class="pa-3">
+    <v-form class="pa-3" @submit.prevent >
+
+
         <div v-if = "status === ''">
             <div v-if = submitted>
                 <p>loading animation</p>
@@ -8,44 +10,123 @@
                 <v-text-field
                     label="Card Number"
                     placeholder="5500 0000 0000 0004"
+                    class="stripe-element card-number field"
+                    ref="cardNumber"
+                    :stripe="stripeKey"
                     required
                     outline
                 ></v-text-field>
                 <v-text-field
                     label="Expiration Date"
-                    placeholder=""
+                    class="stripe-element card-expiry field"
+                    ref="cardExpiry"
+                    :stripe="stripeKey"
+                    placeholder="MM/YY"
                     required
                     outline
                 ></v-text-field>
                 <v-text-field
                     label="CVC"
+                    class="stripe-element card-cvc field"
+                    ref="cardCvc"
+                    :stripe="stripeKey"
                     placeholder="444"
                     required
                     outline
                 ></v-text-field>
             </div>
-            <v-btn color="primary">Buy</v-btn>
+            <div v-else-if = "status === 'success'">
+                <p>success animation</p>
+            </div> 
+            <div class="text-xs-center">
+                <v-btn color="primary">Buy {{ 1700 | dollar }}</v-btn>
+            </div>
         </div>
         <div v-else>
             <p>Error</p>
-        </div>       
+        </div> 
+     
 
-                
-        
+                    
     </v-form>
 
 
 </template>
 
 <script>
+    import {CardNumber,
+            CardExpiry,
+            CardCvc,
+            createToken
+    } from 'vue-stripe-elements-plus';
+    import axios from 'axios';
     export default {
         data () {
             return {
+                response: '',
                 status: '',
-                submitted: false
+                submitted: false,
+                number: false,
+                expiry: false,
+                cvc: false,
+                token: null,
+                stripeKey: process.env.VUE_APP_MYSTRIPEKEY,
+                url: process.env.VUE_APP_URL,
+                StripeName: '',
+                StripeAddressLine1: '',
+                StripeCity: '',
+                StripeCountry: '',
+                StripeCounty: '',
+                StripePostalCode: '',
+                StripeEmail: '',
+                metadata: {},
+            }
+        },
+        filters: {
+            dollar (amount) {
+                return `$${amount / 100}`
+            }
+        },
+        components: { CardNumber, CardExpiry, CardCvc},
+        methods: {
+            pay() {
+                createToken().then(data => {
+                    this.submitted = true;
+                    console.log(data.token);
+                    axios
+                    .post(
+                        this.url,
+                        {
+                            stripeEmail: this.StripeEmail,
+                            stripeToken: data.token.id,
+                            stripeAmt: '1700',
+                            metadata: this.metadata,
+                            stripeShippingName: this.StripeName,
+                            stripeShippingAddressLine1: this.StripeAddressLine1,
+                            shippingAddressstate: this.StripeCounty,
+                            stripeShippingAddressCity: this.StripeCity,
+                            stripeShippingAddressCountry: this.StripeCountry
+                        },
+                        {
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                        }
+                    )
+                    .then(response => {
+                        this.status = 'success';
+                        this.submitted = false;
+                        this.response = JSON.stringify(response, null, 2);
+                        console.log(this.response);
+                    })
+                    .catch(error => {
+                        this.status = 'failure';
+                        this.response = 'Error: ' + JSON.stringify(error, null, 2);
+                    });
+                });
             }
         }
-    }
+    };
 
 
 </script>
